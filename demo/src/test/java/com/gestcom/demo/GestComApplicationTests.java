13597a68abc39a9ee88859d7be96d839521c87cb
Ngatchoufred <friche524@gmail.Com>
2024-09-11 17:01:51 +0200

    Création des entitées User et role

---
+++ demo/src/test/java/com/gestcom/demo/GestComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestcom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
